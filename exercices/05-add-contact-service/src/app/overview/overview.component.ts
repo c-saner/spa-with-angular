32d5104dd0236ab104dd0bfd37e2c70f9c291361
c-saner <41372849+c-saner@users.noreply.github.com>
2019-03-24 11:21:50 +0100

    Add content for exercice 05

---
+++ exercices/05-add-contact-service/src/app/overview/overview.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ContactService } from '../contact.service';
+
+@Component({
+  selector: 'app-overview',
+  templateUrl: './overview.component.html'
+})
+export class OverviewComponent implements OnInit {
+
+  constructor(private readonly contactSerive: ContactService) { }
+
+  ngOnInit() {
+    this.contactSerive.getContactList();
+  }
+
+}
