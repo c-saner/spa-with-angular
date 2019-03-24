ca330b0476e5effb9468a9ec1cab31bcffaa50ef
c-saner <41372849+c-saner@users.noreply.github.com>
2019-03-24 14:17:28 +0100

    Add content for exercice 18

---
+++ exercices/18-navigation-contact-page/src/app/add-contact/add-contact.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddContactComponent } from './add-contact.component';
+
+@NgModule({
+  declarations: [AddContactComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class AddContactModule { }
