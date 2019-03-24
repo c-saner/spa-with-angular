fb5d816ade1a1017c4d062fd2693f813d9bf71eb
c-saner <41372849+c-saner@users.noreply.github.com>
2019-03-24 12:08:13 +0100

    Add content of exercice 12

---
+++ exercices/12-install-angular-material/src/app/overview/overview.component.spec.ts
@@ -0,0 +1,45 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { OverviewComponent } from './overview.component';
+import { ContactService } from 'shared/contact.service';
+import { of } from 'rxjs';
+
+describe('OverviewComponent', () => {
+  let component: OverviewComponent;
+  let fixture: ComponentFixture<OverviewComponent>;
+
+  const mockContactService = jasmine.createSpyObj('ContactService', ['getContactList']);
+  const getContactListSpy = mockContactService.getContactList.and.returnValue( of([]) );
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      declarations: [ OverviewComponent ],
+      providers:    [
+        { provide: ContactService, useValue: mockContactService }
+      ]
+    })
+    .compileComponents();
+  }));
+
+  afterEach(() => {
+    getContactListSpy.calls.reset();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OverviewComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call getContactList of ContactService on init', () => {
+    expect(getContactListSpy).toHaveBeenCalledTimes(1);
+  });
+});
---
+++ exercices/12-install-angular-material/src/app/shared/contact.service.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ContactService } from './contact.service';
+
+describe('ContactService', () => {
+  let httpTestingController: HttpTestingController;
+  let service: ContactService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
+    httpTestingController = TestBed.get(HttpTestingController);
+    service = TestBed.get(ContactService);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should call getContactList with URL http://localhost:3000/contacts', () => {
+    service.getContactList().subscribe(
+      contactList => expect(contactList.length).toBe(0)
+    );
+
+    const req = httpTestingController.expectOne('http://localhost:3000/contacts');
+    expect(req.request.method).toBe('GET');
+    req.flush([]);
+  });
+});
